9b3db3734bdadd932a4c6f00a936d31387e92f6d
Jafeth <alesol.absa@gmail.com>
2024-10-25 16:20:11 -0600

    fix(jovenes)

--- flutter_button/lib/jovenes.dart
+++ flutter_button/lib/jovenes.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'package:audioplayers/audioplayers.dart'; // Para el sonido del tic-tac
+import 'package:audioplayers/audioplayers.dart';
 
 class JovenesScreen extends StatefulWidget {
   const JovenesScreen({Key? key}) : super(key: key);
@@ -12,44 +12,184 @@ class JovenesScreen extends StatefulWidget {
 class _JovenesScreenState extends State<JovenesScreen> with TickerProviderStateMixin {
   Timer? _timer;
   int _currentQuestionIndex = 0;
-  final AudioPlayer _audioPlayer = AudioPlayer(); // Control del audio del tic-tac
-  String _currentTime = ''; // Variable para mostrar la hora actual
-  bool _isRunning = true; // Estado del temporizador
-  bool _isPaused = false; // Estado de pausa del temporizador
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  String _currentTime = '';
+  bool _isRunning = true;
+  bool _showCorrect = false;
+  bool _showIncorrect = false;
+  bool _optionSelected = false; // Variable para bloquear selección adicional
 
-  final List<Map<String, dynamic>> _questions = [
-    {
-      'question': '¿Cuántos libros tiene la Biblia?',
-      'icon': Icons.book,
-    },
-    {
-      'question': '¿Cómo se clasifican los libros de la Biblia?',
-      'icon': Icons.library_books,
-    },
-    {
-      'question': '¿Qué día creó Dios la luna y el sol?',
-      'icon': Icons.wb_sunny,
-    },
-    {
-      'question': '¿Cuál fue el primer nombre de Abraham?',
-      'icon': Icons.person,
-    },
-  ];
+
+
+final List<Map<String, dynamic>> _questions = [
+  {
+    'question': '¿Cuántos libros tiene la Biblia?',
+    'answer': '66',
+    'options': ['72', '39', '66', '58'],
+  },
+  {
+    'question': '¿Cómo se clasifican los libros de la Biblia?',
+    'answer': 'El Pentateuco, Los Históricos, Los Poéticos, Los Proféticos, Los Evangelios',
+    'options': [
+      'Libros mayores y menores',
+      'Libros de ley y poéticos',
+      'Evangelios y apocalípticos',
+      'El Pentateuco, Los Históricos, Los Poéticos, Los Proféticos, Los Evangelios'
+    ],
+  },
+  {
+    'question': '¿Qué día creó Dios la luna y el sol?',
+    'answer': 'Cuarto día',
+    'options': ['Primer día', 'Segundo día', 'Cuarto día', 'Sexto día'],
+  },
+  {
+    'question': '¿Cuál fue el primer nombre de Abraham?',
+    'answer': 'Abram',
+    'options': ['Isaac', 'Jacob', 'Abram', 'David'],
+  },
+  {
+    'question': '¿Quién fue la mujer que fue convertida en la estatua de sal?',
+    'answer': 'Edith',
+    'options': ['Sara', 'Edith', 'Rebeca', 'Raquel'],
+  },
+  {
+    'question': '¿Cuántos años trabajó Jacob por Raquel?',
+    'answer': '14 años',
+    'options': ['7 años', '14 años', '20 años', '30 años'],
+  },
+  {
+    'question': '¿Cómo se llamaban los hijos de José?',
+    'answer': 'Efraín y Manasés',
+    'options': ['Efraín y Manasés', 'Isaac y Jacob', 'Caín y Abel', 'Jacob y Esaú'],
+  },
+  {
+    'question': '¿Cómo se le presentó Dios a Moisés?',
+    'answer': 'Por medio de una zarza ardiente',
+    'options': ['Como un fuego en el cielo', 'Por medio de una zarza ardiente', 'Con un ángel', 'Como un viento fuerte'],
+  },
+  {
+    'question': '¿Por qué plaga murieron los primogénitos en Egipto?',
+    'answer': 'Plaga de mortandad',
+    'options': ['Plaga de ranas', 'Plaga de mortandad', 'Plaga de langostas', 'Plaga de oscuridad'],
+  },
+  {
+    'question': '¿Quién hizo el becerro de oro?',
+    'answer': 'Aarón',
+    'options': ['Moisés', 'Aarón', 'Josué', 'Caleb'],
+  },
+  {
+    'question': '¿Qué tribu fue consagrada para el sacerdocio?',
+    'answer': 'La tribu de Levi',
+    'options': ['La tribu de Judá', 'La tribu de Efraín', 'La tribu de Levi', 'La tribu de Rubén'],
+  },
+  {
+    'question': '¿Qué cubría al pueblo de Israel en el día y en la noche?',
+    'answer': 'Una nube en el día y una apariencia de fuego en la noche',
+    'options': [
+      'Una nube en el día y una apariencia de fuego en la noche',
+      'Una nube en el día y un arco iris en la noche',
+      'Una estrella en la noche y el sol en el día',
+      'La luna en la noche y el sol en el día'
+    ],
+  },
+  {
+    'question': '¿Por qué a María, la hermana de Moisés, le dio lepra?',
+    'answer': 'Por murmurar contra Moisés',
+    'options': ['Por no creer en Dios', 'Por murmurar contra Moisés', 'Por mentir', 'Por tocar el Arca'],
+  },
+  {
+    'question': '¿Cuántos espías envió Moisés a la tierra prometida?',
+    'answer': '12 espías',
+    'options': ['10 espías', '12 espías', '8 espías', '15 espías'],
+  },
+  {
+    'question': '¿Por qué Moisés no entró a la tierra prometida?',
+    'answer': 'Por desobediente',
+    'options': ['Por falta de fe', 'Por miedo', 'Por desobediente', 'Por cansancio'],
+  },
+  {
+    'question': '¿A qué hombre le habló una burra?',
+    'answer': 'A Balaam',
+    'options': ['A Moisés', 'A Aarón', 'A Balaam', 'A David'],
+  },
+  {
+    'question': '¿Qué significa Deuteronomio?',
+    'answer': 'Segunda entrega de la ley',
+    'options': ['Ley de los sacerdotes', 'Instrucción a los reyes', 'Ley de los profetas', 'Segunda entrega de la ley'],
+  },
+  {
+    'question': '¿Cuántos años pasó el pueblo de Israel en el desierto?',
+    'answer': '40 años',
+    'options': ['20 años', '30 años', '40 años', '50 años'],
+  },
+  {
+    'question': '¿Cuál es el evangelio universal?',
+    'answer': 'El evangelio según San Juan',
+    'options': [
+      'El evangelio según San Mateo',
+      'El evangelio según San Marcos',
+      'El evangelio según San Lucas',
+      'El evangelio según San Juan'
+    ],
+  },
+  {
+    'question': '¿Qué persona escribió más en el Nuevo Testamento?',
+    'answer': 'Lucas',
+    'options': ['Pablo', 'Lucas', 'Pedro', 'Juan'],
+  },
+  {
+    'question': '¿Quién de los discípulos de Jesús fue un cobrador de impuestos?',
+    'answer': 'Mateo',
+    'options': ['Juan', 'Pedro', 'Mateo', 'Tomás'],
+  },
+  {
+    'question': '¿Cuántos versículos tiene el salmo 119?',
+    'answer': '176 versículos',
+    'options': ['150 versículos', '176 versículos', '100 versículos', '200 versículos'],
+  },
+  {
+    'question': '¿Quién escribió el libro de Hechos?',
+    'answer': 'Lucas',
+    'options': ['Pablo', 'Lucas', 'Pedro', 'Juan'],
+  },
+  {
+    'question': '¿Qué significa la palabra Biblia?',
+    'answer': 'Colección de libros',
+    'options': ['Libro sagrado', 'Colección de libros', 'Libro de la ley', 'Historia de Dios'],
+  },
+  {
+    'question': '¿En qué género literario fue escrito el libro de Job?',
+    'answer': 'En poesía (prosa)',
+    'options': ['Narrativa', 'Historia', 'En poesía (prosa)', 'En ley'],
+  },
+  {
+    'question': '¿De cuántos capítulos se compone el Sermón del monte según Mateo?',
+    'answer': 'De 3 capítulos 5-6 y 7',
+    'options': [
+      'De 2 capítulos 5-6',
+      'De 3 capítulos 5-6 y 7',
+      'De 4 capítulos 5-6-7-8',
+      'De 1 capítulo 5'
+    ],
+  },
+];
+
+
+
+  final List<String> _optionLabels = ['A', 'B', 'C', 'D'];
 
   @override
   void initState() {
     super.initState();
-    _questions.shuffle(); // Revolver preguntas aleatoriamente
-    _startTickTockSound(); // Iniciar sonido de tic-tac
-    _startTimer(); // Iniciar el temporizador
+    _questions.shuffle();
+    _startTickTockSound();
+    _startTimer();
   }
 
-  // Función para iniciar el sonido de tic-tac
   void _startTickTockSound() async {
-    await _audioPlayer.play('assets/ticking_clock.mp3', isLocal: true); // Reproducir sonido local
+    await _audioPlayer.play('assets/ticking_clock.mp3', isLocal: true);
   }
 
-  // Función para iniciar el temporizador
   void _startTimer() {
     _timer = Timer.periodic(const Duration(milliseconds: 1), (Timer timer) {
       final now = DateTime.now();
@@ -62,7 +202,6 @@ class _JovenesScreenState extends State<JovenesScreen> with TickerProviderStateM
     });
   }
 
-  // Detener el sonido y temporizador cuando se deja de usar la pantalla
   @override
   void dispose() {
     _audioPlayer.stop();
@@ -70,37 +209,45 @@ class _JovenesScreenState extends State<JovenesScreen> with TickerProviderStateM
     super.dispose();
   }
 
-  // Navegar a la pregunta siguiente
-  void _nextQuestion() {
-    if (_currentQuestionIndex < _questions.length - 1) {
+  void _checkAnswer(String selectedOption) {
+    if (!_optionSelected) {
       setState(() {
-        _currentQuestionIndex++;
-      });
-    }
-  }
-
-  // Navegar a la pregunta anterior
-  void _previousQuestion() {
-    if (_currentQuestionIndex > 0) {
-      setState(() {
-        _currentQuestionIndex--;
-      });
-    }
-  }
-
-  // Alternar el estado del temporizador (pausar/continuar)
-  void _toggleTimer() {
-    setState(() {
-      if (_isRunning) {
+        _optionSelected = true; // Bloquea la selección adicional
         _isRunning = false;
-        _isPaused = true;
-        _timer?.cancel();
-      } else {
-        _isRunning = true;
-        _isPaused = false;
-        _startTimer();
+        if (selectedOption == _questions[_currentQuestionIndex]['answer']) {
+          _showCorrect = true;
+          _showIncorrect = false;
+        } else {
+          _showCorrect = false;
+          _showIncorrect = true;
+        }
+      });
+    }
+  }
+
+  void _nextQuestion() {
+    setState(() {
+      if (_currentQuestionIndex < _questions.length - 1) {
+        _currentQuestionIndex++;
+        _resetQuestionState();
       }
     });
+  }
+
+  void _previousQuestion() {
+    setState(() {
+      if (_currentQuestionIndex > 0) {
+        _currentQuestionIndex--;
+        _resetQuestionState();
+      }
+    });
+  }
+
+  void _resetQuestionState() {
+    _showCorrect = false;
+    _showIncorrect = false;
+    _isRunning = true;
+    _optionSelected = false; // Reinicia el bloqueo para la nueva pregunta
   }
 
   @override
@@ -113,27 +260,25 @@ class _JovenesScreenState extends State<JovenesScreen> with TickerProviderStateM
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Pregunta actual con animación
+            Text(
+              _currentTime,
+              style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
             AnimatedSwitcher(
               duration: const Duration(milliseconds: 500),
-              transitionBuilder: (Widget child, Animation<double> animation) {
-                return ScaleTransition(scale: animation, child: child);
-              },
               child: Column(
                 key: ValueKey<int>(_currentQuestionIndex),
                 children: [
-                  // Ícono alusivo a la pregunta
                   Icon(
-                    _questions[_currentQuestionIndex]['icon'] as IconData,
+                    Icons.question_mark,
                     size: 60,
                     color: const Color(0xFF4E91FF),
                   ),
                   const SizedBox(height: 20),
-                  // Texto de la pregunta
                   Text(
-                    _questions[_currentQuestionIndex]['question']!,
+                    _questions[_currentQuestionIndex]['question'],
                     style: const TextStyle(
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
@@ -141,62 +286,53 @@ class _JovenesScreenState extends State<JovenesScreen> with TickerProviderStateM
                     ),
                     textAlign: TextAlign.center,
                   ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            // Temporizador
-            Text(
-              _currentTime,
-              style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 30),
-            // Botón para pausar o continuar el temporizador con diseño de botón rojo
-            GestureDetector(
-              onTap: _toggleTimer,
-              child: Container(
-                width: 150,
-                height: 150,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: const LinearGradient(
-                    colors: [Colors.red, Colors.redAccent],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
+                  const SizedBox(height: 30),
+                  Column(
+                    children: (_questions[_currentQuestionIndex]['options'] as List<String>)
+                        .asMap()
+                        .entries
+                        .map((entry) {
+                      int index = entry.key;
+                      String option = entry.value;
+                      return GestureDetector(
+                        onTap: () => _checkAnswer(option),
+                        child: Container(
+                          width: double.infinity,
+                          margin: const EdgeInsets.symmetric(vertical: 10),
+                          padding: const EdgeInsets.all(15),
+                          decoration: BoxDecoration(
+                            color: _showCorrect && option == _questions[_currentQuestionIndex]['answer']
+                                ? Colors.green
+                                : _showIncorrect && option != _questions[_currentQuestionIndex]['answer']
+                                    ? Colors.red
+                                    : const Color(0xFF293B5F),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Row(
+                            children: [
+                              Text(
+                                "${_optionLabels[index]}.",
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(width: 10),
+                              Expanded(
+                                child: Text(
+                                  option,
+                                  style: const TextStyle(color: Colors.white, fontSize: 18),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    }).toList(),
                   ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.6),
-                      spreadRadius: 5,
-                      blurRadius: 20,
-                      offset: const Offset(0, 5),
-                    ),
-                  ],
-                  border: Border.all(
-                    color: Colors.white,
-                    width: 3,
-                  ),
-                ),
-                child: Center(
-                  child: Text(
-                    _isPaused ? 'Reanudar' : 'Detener',
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 30),
-            // Botones de navegación (Anterior/Siguiente) alineados abajo
-            Expanded(
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 20.0),
-                  child: Row(
+                  const SizedBox(height: 30),
+                  Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       if (_currentQuestionIndex > 0)
@@ -238,7 +374,7 @@ class _JovenesScreenState extends State<JovenesScreen> with TickerProviderStateM
                       ),
                     ],
                   ),
-                ),
+                ],
               ),
             ),
           ],
